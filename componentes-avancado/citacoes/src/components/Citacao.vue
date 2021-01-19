5e5bc3b3dc049df6d607b6bf41848e3e00a33216
Mauricio <gomes98@gmail.com>
2021-01-19 20:04:23 -0300

    Slots Nomeados

--- componentes-avancado/citacoes/src/components/Citacao.vue
+++ componentes-avancado/citacoes/src/components/Citacao.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="citacao">
-    <slot></slot>
+    <slot name="autor"></slot>
+    <slot name="texto"></slot>
+    <slot name="fonte"></slot>
   </div>
 </template>
 
@@ -15,5 +17,9 @@ export default {
   background-color: rgba(0, 0, 0, 0.1);
   padding: 25px;
   max-width: 450px;
+}
+
+h1{
+    color: blue;
 }
 </style>
