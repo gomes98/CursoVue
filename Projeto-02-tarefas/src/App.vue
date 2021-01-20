a2248717abbd9ca221b6ec470d163761b1d5a361
Mauricio <gomes98@gmail.com>
2021-01-19 23:59:49 -0300

    Projeto Task Iniciado

---
+++ Projeto-02-tarefas/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid 
+	:tasks="tasks" 
+	@taskDeleted1="deleteTask" 
+	@taskChanged="changeTask"/>
+  </div>
+</template>
+
+<script>
+import TaskGrid from "./components/TaskGrid.vue";
+import NewTask from "./components/NewTask.vue";
+export default {
+  components: {
+    TaskGrid,
+    NewTask,
+  },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar Louca", pending: true },
+        { name: "Comprar Ps5", pending: false },
+      ],
+    };
+  },
+  methods: {
+    addTask(task) {
+      const sameName = (t) => t.name == task.name;
+      const reallyNew = this.tasks.filter(sameName).length == 0;
+      if (reallyNew) {
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true,
+        });
+      }
+    },
+    deleteTask(i) {
+      this.tasks.splice(i, 1);
+    },
+    changeTask(i) {
+      this.tasks[i].pending = !this.tasks[i].pending;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
+
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
+</style>
