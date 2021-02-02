af7eb282f0af923f14f6c7ce801794e4d8ff9cb8
Mauricio <gomes98@gmail.com>
2021-02-01 21:16:16 -0300

    Stock Trader Iniciado

---
+++ Projeto-03-StockTrader/stock-trader/src/components/stocks/Stock.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-flex class="pr-3 pb-3">
+    <v-card class="green darker-3 white--text">
+      <v-card class="headline">
+
+        <strong>Nome <small>(Preço: PRECO)</small></strong>
+      </v-card>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
