2b30c8689b8e3539d85cba70c20a33ff78a6be59
Mauricio <gomes98@gmail.com>
2021-02-25 19:24:37 -0300

    Projeto 5 Concluido

---
+++ Projeto-05-Monty_Hall/montyhall/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+      <h1>Problema de Monty Hall</h1>
+      <div class="form">
+          <div v-if="!started">
+              <label for="portsAmount">Quantas Portas?</label>
+              <input type="text" id="portsAmount" v-model.number="portsAmount">
+          </div>
+          <div v-if="!started">
+              <label for="selectedPort">Porta Premiada</label>
+              <input type="text" id="selectedPort" v-model.number="selectedPort">
+          </div>
+          <button v-if="!started" @click="started = true">Iniciar</button>
+          <button v-if="started" @click="started = false">Reiniciar</button>
+      </div>
+      <div class="doors" v-if="started">
+          <div v-for="i in portsAmount" :key="i">
+                <Door :number="i" :hasGift="i == selectedPort"/>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Door  from './components/Door'
+export default {
+    name: "App",
+    components:{ Door },
+    data: ()=>{
+        return{
+            started: false,
+            portsAmount: 3,
+            selectedPort: null
+        }
+    }
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+    font-family: 'Montserrat', 'sans-serif';
+}
+
+body{
+    color: #fff;
+    background: linear-gradient(to right, #00b09b, #96c93d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+#app h1{
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+}
+
+.form{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button{
+    margin-bottom: 10px;
+    font-size: 2rem;
+} 
+
+.doors{
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+</style>
