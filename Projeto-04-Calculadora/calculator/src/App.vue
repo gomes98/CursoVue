c2bec1db6d03e365b8112e842b2e2466790ab8b0
Mauricio <gomes98@gmail.com>
2021-02-26 08:46:53 -0300

    Renomeado knowledge

---
+++ Projeto-04-Calculadora/calculator/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>aha</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body{
+    margin: 0;
+}
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+
+    background: linear-gradient(to right, #4ecdc4, #556270); 
+}
+</style>
