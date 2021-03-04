67586b283a7e01848760d60dd364365a81e26172
Mauricio <gomes98@gmail.com>
2021-03-04 15:36:43 -0300

    Visualização Artigos Finalizados

--- knowledge/versao-inicial/frontend/src/config/router.js
+++ knowledge/versao-inicial/frontend/src/config/router.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 
 import Home from '../components/home/Home'
 import AdminPages from '../components/admin/AdminPages'
+import ArticlesByCategory from '../components/article/ArticlesByCategory'
+import ArticleById from '../components/article/ArticleById'
 
 Vue.use(VueRouter)
 
@@ -15,6 +17,14 @@ const routes = [{
     name: 'adminPages',
     path: '/admin',
     component: AdminPages
+}, {
+    name: 'articlesByCategoru',
+    path: '/categories/:id/articles',
+    component: ArticlesByCategory
+}, {
+    name: 'articleById',
+    path: '/articles/:id',
+    component: ArticleById
 }]
 
 const router = new VueRouter({
